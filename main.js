5fac105e16a4be5c963f11f4a1d0590decbe2570
gianbarbaro <gianlbarbaro@gmail.com>
2023-01-25 02:32:16 -0300

    diseño responsive

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-const nav = document.querySelector('.navbar');
+/* efecto scroll en el header */
+const nav = document.querySelector('header');
 
 window.addEventListener('scroll', function(){
     nav.classList.toggle('active', this.window.scrollY > 0)
+})
+
+/* navbar resposive */
+const barraMenu = document.getElementById('barra-menu');
+const navMenu = document.getElementById('nav-menu');
+
+barraMenu.addEventListener('click', function(){
+    navMenu.classList.toggle('nav-menu_show')
 })
