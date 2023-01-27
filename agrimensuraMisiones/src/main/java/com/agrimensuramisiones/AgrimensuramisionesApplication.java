abf6c51c26cd43da7c6d7bb5e3affc818cf1f05b
gianbarbaro <gianlbarbaro@gmail.com>
2023-01-27 19:19:14 -0300

    inicio del proyecto con spring

---
+++ agrimensuraMisiones/src/main/java/com/agrimensuramisiones/AgrimensuramisionesApplication.java
@@ -0,0 +1,13 @@
+package com.agrimensuramisiones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrimensuramisionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrimensuramisionesApplication.class, args);
+	}
+
+}
---
+++ agrimensuraMisiones/src/test/java/com/agrimensuramisiones/AgrimensuramisionesApplicationTests.java
@@ -0,0 +1,12 @@
+package com.agrimensuramisiones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrimensuramisionesApplicationTests {
+    
+    @Test
+	void contextLoads() {
+	}
+}
